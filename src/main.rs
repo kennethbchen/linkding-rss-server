3c3b4317bbfabc4333eaed1d8bbae2f5cb17aede
kennethbchen <kennethbchen@gmail.com>
2025-05-07 23:22:56 -0400

    Implemented parsing config

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use config::Config;
 
-struct AppSettings {
+struct AppConfig {
     api_key: String,
     url: String,
 }
 
-fn parse_config() {
+fn parse_config() -> AppConfig {
     let Ok(config) = Config::builder()
         .add_source(config::File::with_name("config.toml"))
         .build()
@@ -16,13 +16,20 @@ fn parse_config() {
     let Ok(api_key) = &config.get_string("api_key") else {
         panic!("'api_key' not found in config");
     };
+    let api_key: String = api_key.clone();
 
     let Ok(url) = &config.get_string("url") else {
         panic!("'url' not found in config");
     };
-    println!("{}", api_key)
+    let url: String = url.clone();
+
+    return AppConfig { api_key, url };
 }
 
 fn main() {
-    parse_config();
+    let cfg: AppConfig = parse_config();
+
+    println!("{}", cfg.api_key);
+
+    println!("{}", cfg.url);
 }
